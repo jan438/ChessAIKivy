ec0a3f73c7b790aea85ada024c5cf48dbe399ed4
jan438 <jan438@users.noreply.github.com>
2024-03-20 14:25:03 +0100

    Removed screenmanager

--- Window.py
+++ Window.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.config import Config
