d11dc3425b1bf7ccfd2748df562fa7ff83ee5183
jan438 <jan438@users.noreply.github.com>
2024-03-20 15:19:11 +0100

    Zes parameters given to key handler

--- Window.py
+++ Window.py
@@ -716,8 +716,8 @@ class ChessApp(App):
         except PermissionError:
             print("You do not have permissions to change to {0}".format(path))
     
-    def key_action(self, *args):
-        print("got a key event")
+    def key_action(self, key1, key2, key3, key4, key5):
+        print("got a key event", key1, key2, key3, key4, key5)
 
 boardai = boardai.Boardai.new()
 alg = boardai.alg
