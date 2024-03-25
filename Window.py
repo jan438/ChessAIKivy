1857a0f141f466efa3fa77f9cec90ec6ab5d3455
jan438 <jan438@users.noreply.github.com>
2024-03-25 10:38:27 +0100

    Enpassant in new function

--- Window.py
+++ Window.py
@@ -554,10 +554,7 @@ class ChessBoard(RelativeLayout):
                                 self.remove_widget(enemy)
                             ChessBoard.piece_pressed = False
                             ChessBoard.available_moves = {"available_moves":(), "pieces_to_capture":[]}
-                            hmmove = ""+xpos_to_letter(round(old_x))+ypos_to_digit(round(old_y))+" "+xpos_to_letter(grid_x)+ypos_to_digit(grid_y)
-                            move = get_user_move(hmmove)
-                            print("hmmove:",hmmove,"move:",move.xfrom,move.yfrom,move.xto,move.yto)
-                            boardai.perform_move(move)
+                            self.perform_ai_move(round(old_x), round(old_y), grid_x, grid_y)
                             self.draw_moves()
             else:
                 try:
