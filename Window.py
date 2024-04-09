3ee0f63182176b7d36a4ac74dfc525cb5dca788a
jan438 <jan438@users.noreply.github.com>
2024-04-09 10:07:23 +0200

    Print added no pieses left right

--- Window.py
+++ Window.py
@@ -337,6 +337,7 @@ class King(ChessPiece):
                     no_piece_right = False
                 elif piece.grid_y == self.grid_y and piece.grid_x < self.grid_x and (piece.id[5:9] != "Rook" or self.id[:5] != piece.id[:5]):
                     no_piece_left = False
+            print("Coordinates", self.grid_x, self.grid_y, "left", no_piece_left, "right", no_piece_right)
             if no_piece_right and no_piece_left:
                 return [(self.grid_x-2, self.grid_y),(self.grid_x+2, self.grid_y)]
             if no_piece_right:
