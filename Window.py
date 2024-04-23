16e16b49affe9427c415274b59f447855968656b
jan438 <jan438@users.noreply.github.com>
2024-04-23 18:40:22 +0200

    id changed

--- Window.py
+++ Window.py
@@ -700,10 +700,10 @@ class ChessBoard(RelativeLayout):
                     self.perform_ai_move(round(old_x), round(old_y), grid_x, grid_y)
                     if grid_y == 7 and child.id[0:9] == "WhitePawn":
                         self.remove_widget(child)
-                        self.add_widget(Queen(id="WhiteQueen",source="Assets/PNG/WhiteQueen.png", grid_x=grid_x, grid_y=grid_y))
+                        self.add_widget(Queen(id="WhiteQueen2",source="Assets/PNG/WhiteQueen.png", grid_x=grid_x, grid_y=grid_y))
                     if grid_y == 0 and child.id[0:9] == "BlackPawn":
                         self.remove_widget(child)
-                        self.add_widget(Queen(id="BlackQueen",source="Assets/PNG/BlackQueen.png", grid_x=grid_x, grid_y=grid_y))
+                        self.add_widget(Queen(id="BlackQueen2",source="Assets/PNG/BlackQueen.png", grid_x=grid_x, grid_y=grid_y))
                     ai_move = self.let_ai_move()
                     if child.id[5:9] == "Pawn" and abs(grid_y - old_y) == 2:
                         self.mark_en_passant(child.id[:5], grid_x)
@@ -728,10 +728,10 @@ class ChessBoard(RelativeLayout):
                             self.perform_ai_move(round(old_x), round(old_y), grid_x, grid_y)
                             if grid_y == 7 and child.id[0:9] == "WhitePawn":
                                 self.remove_widget(child)
-                                self.add_widget(Queen(id="WhiteQueen",source="Assets/PNG/WhiteQueen.png", grid_x=grid_x, grid_y=grid_y))
+                                self.add_widget(Queen(id="WhiteQueen2",source="Assets/PNG/WhiteQueen.png", grid_x=grid_x, grid_y=grid_y))
                             if grid_y == 0 and child.id[0:9] == "BlackPawn":
                                 self.remove_widget(child)
-                                self.add_widget(Queen(id="BlackQueen",source="Assets/PNG/BlackQueen.png", grid_x=grid_x, grid_y=grid_y))
+                                self.add_widget(Queen(id="BlackQueen2",source="Assets/PNG/BlackQueen.png", grid_x=grid_x, grid_y=grid_y))
                             ai_move = self.let_ai_move() 
                             if (child.id[5:9] == "Pawn" or child.id[5:9] == "Rook" or child.id[5:9] == "King") and child.First_use:
                                 child.First_use = False
