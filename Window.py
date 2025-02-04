55ff10159abbcbf2a64c88432bf4ee2233b6dcc8
jan438 <jan438@users.noreply.github.com>
2025-02-04 09:59:35 +0100

    imprt audio added

--- Window.py
+++ Window.py
@@ -2,11 +2,12 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
+from kivy.core.audio import SoundLoader
 from kivy.uix.widget import Widget
 from kivy.config import Config
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
