022b5471a9f08945c09ecd14a361492db731e10e
jan438 <jan438@users.noreply.github.com>
2025-02-08 14:52:26 +0100

    enpassant whitr completed

--- Window.py
+++ Window.py
@@ -570,6 +570,10 @@ class ChessBoard(RelativeLayout):
             if child.id[5:9] == "Pawn" and abs(move.xfrom - move.xto) == 1 and abs(move.yfrom - move.yto) == 1 and move.yto == 2:
                 anim = Animation(grid_x = move.xto, grid_y = 5, t='in_out_expo', duration=0.5)
                 anim.start(child)
+                ChessBoard.piece_index = ChessBoard.pieceindex_at_board(self, move.xto, 3)
+                if ChessBoard.piece_index > -1:
+                    child = self.children[ChessBoard.piece_index]
+                    self.remove_widget(child)
             if child.id[5:9] == "Pawn" and abs(move.xfrom - move.xto) == 1 and abs(move.yfrom - move.yto) == 1 and move.yto == 5:
                 anim = Animation(grid_x = move.xto, grid_y = 2, t='in_out_expo', duration=0.5)
                 anim.start(child)
