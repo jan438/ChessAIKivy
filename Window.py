6f1c351a5c252d191b9498626a38c94e363737ad
jan438 <jan438@users.noreply.github.com>
2025-02-15 10:57:36 +0100

    pawn can cot strike

--- Window.py
+++ Window.py
@@ -620,7 +620,7 @@ class ChessBoard(RelativeLayout):
         aiposy = move.yfrom
         if deltax == 0 and deltay == 2 and color == "White" and move.yfrom == 6 and boardai.chesspiecesai[aiposx][aiposy - 1] == 0 and boardai.chesspiecesai[aiposx][aiposy - 2] == 0:
             return True
-        if deltax == 0 and deltay == 1 and color == "White":
+        if deltax == 0 and deltay == 1 and color == "White" and boardai.chesspiecesai[aiposx][aiposy - 1] == 0:
             return True
         if deltax == 0 and deltay == -2 and color == "Black" and move.yfrom == 1 and boardai.chesspiecesai[aiposx][aiposy + 1] == 0:
             return True
