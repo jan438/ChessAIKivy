34741d27c8c1c2bc2480136ac0b4fc55244b1167
jan438 <jan438@users.noreply.github.com>
2025-02-15 15:03:52 +0100

    black pawn pro added

--- Window.py
+++ Window.py
@@ -631,7 +631,9 @@ class ChessBoard(RelativeLayout):
             if boardai.chesspiecesai[move.xto][move.yto] != 0:
                 return True
         if move.yto == 0 and (move.xfrom - move.xto) == 0 and color == "White" and boardai.chesspiecesai[move.xto][move.yto] == 0:
-            return True 
+            return True
+        if move.yto == 7 and (move.xfrom - move.xto) == 0 and color == "Black" and boardai.chesspiecesai[move.xto][move.yto] == 0:
+            return True
         return False
         
     def valid_king(self, move, color):
